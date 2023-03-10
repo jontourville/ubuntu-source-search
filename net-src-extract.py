5ef602d522fcfdee512bc1070dfc34e378b3e7de
Jon Tourville <jon.tourville@canonical.com>
2023-03-10 06:37:41 -0600

    Add arg parsing

--- net-src-extract.py
+++ net-src-extract.py
@@ -1,20 +1,26 @@
 #!/usr/bin/env python3
 
+import sys
+import argparse
 import collections
 import urllib.request
 import gzip
 
-base_url = "http://archive.ubuntu.com"
-release = "kinetic"
-repo = "restricted"
+def get_args():
+    mirror = "http://archive.ubuntu.com"
+    parser = argparse.ArgumentParser()
+    parser.add_argument("RELEASE", help='Ubuntu release codename (ex. jammy)')
+    parser.add_argument("REPOSITORY", help='repository name (main, universe, multiverse, or restricted)')
+    parser.add_argument("-m", "--mirror", default=mirror, help="URL of mirror (defaults to %s)" % mirror)
+    return parser.parse_args()
 
 SourceArchive = collections.namedtuple("SourceArchive", ["url", "size", "package"])
 
-def get_source_urls(base_url, release, repo):
-    sources_url = "%s/ubuntu/dists/%s/%s/source/Sources.gz" % (base_url, release, repo)
+def get_archive_list(mirror, release, repo):
+    sources_url = "%s/ubuntu/dists/%s/%s/source/Sources.gz" % (mirror, release, repo)
     sources = gzip.decompress(urllib.request.urlopen(sources_url).read()).decode().split("\n")
 
-    package_urls = {}
+    archives = []
     package = ""
     directory = ""
     is_file_line = False
@@ -40,13 +46,34 @@ def get_source_urls(base_url, release, repo):
             continue
 
         filename = line.split()[2]
+        size = int(line.split()[1])
         exts = filename.split(".")
 
         if len(exts) > 1 and "tar" in exts and (exts[-1] == "tar" or exts[-2] == "tar"):
-            urls = package_urls.get(package, [])
-            urls.append("%s/%s/%s" % (base_url, directory, filename))
-            package_urls[package] = urls
+            url = "%s/ubuntu/%s/%s" % (mirror, directory, filename)
+            archives.append(SourceArchive(url, size, package))
 
-    return package_urls
+    return archives
 
-package_urls = get_source_urls(base_url, release, repo)
+def size_to_str(size):
+    if size < 1024:
+        return "%d" % (size)
+    elif size < 1024 * 1024:
+        return "%.1fK" % (size / 1024.0)
+    elif size < 1024 * 1024 * 1024:
+        return "%.1fM" % (size / 1024.0 / 1024.0)
+    elif size < 1024 * 1024 * 1024 * 1024:
+        return "%.1fG" % (size / 1024.0 / 1024.0 / 1024.0)
+    return "%.1fT" % (size / 1024.0 / 1024.0 / 1024.0 / 1024.0)
+
+if __name__ == "__main__":
+    args = get_args()
+
+    archives = get_archive_list(args.mirror, args.RELEASE, "main")
+
+    total_size = 0
+    for archive in archives:
+        total_size += archive.size
+
+    print("Number of archives: %d" % (len(archives)))
+    print("Compressed size:    %s" % (size_to_str(total_size)))
